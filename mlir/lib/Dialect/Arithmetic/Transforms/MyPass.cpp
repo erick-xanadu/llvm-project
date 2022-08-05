6c14b2872e6ce82951a5f185950feccd3bc60048
Erick Eduardo Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2022-08-05 20:09:32 +0000

    Prototype for AddIOp to GenericOp.
    This prototype actually transforms AddIOp with tensor operands into
    GenericOp with tensor operands.

--- mlir/lib/Dialect/Arithmetic/Transforms/MyPass.cpp
+++ mlir/lib/Dialect/Arithmetic/Transforms/MyPass.cpp
@@ -11,10 +11,41 @@
 #include "PassDetail.h"
 #include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/Arithmetic/Transforms/Passes.h"
+#include "mlir/Dialect/Linalg/IR/Linalg.h"
+#include "mlir/IR/TypeUtilities.h"
 #include "mlir/Transforms/DialectConversion.h"
 
 using namespace mlir;
 using namespace mlir::arith;
+using namespace mlir::linalg;
+
+static SmallVector<StringRef>
+getNParallelLoopsAttrs(unsigned nParallelLoops) {
+  return SmallVector<StringRef>(nParallelLoops, getParallelIteratorTypeName());
+}
+
+static SmallVector<Value>
+condenseValues(const SmallVector<Value> &values) {
+  SmallVector<Value> condensedValues;
+  for (auto value : values)
+    if (value)
+      condensedValues.push_back(value);
+  return condensedValues;
+}
+
+static Value
+createLinalgBodyCalculationForElementwiseOp(Operation *op, ValueRange args,
+                                            ArrayRef<Type> resultTypes,
+                                            PatternRewriter &rewriter) {
+  Location loc = op->getLoc();
+  auto elementTy =
+      op->getOperand(0).getType().cast<ShapedType>().getElementType();
+
+  if (isa<arith::AddIOp>(op) && elementTy.isa<IntegerType>())
+    return rewriter.create<arith::AddIOp>(loc, resultTypes, args);
+
+  return nullptr;
+}
 
 static bool isaTensor(Type t) { return t.isa<TensorType>(); }
 
@@ -35,8 +66,113 @@ struct ConvertAddIOpToSubIOp : public ConversionPattern {
 
     void rewrite(Operation *op, ArrayRef<Value> operands,
 		    ConversionPatternRewriter &rewriter) const final {
-	    rewriter.replaceOpWithNewOp <SubIOp> (op, op->getResultTypes (),
-			    operands, op->getAttrs ());
+
+  auto loc = op->getLoc();
+
+  assert(op->getNumResults() == 1 &&
+         "All TOSA elementwise ops should only return a single result.");
+
+  auto results = op->getResults();
+  auto resultTy = op->getResult(0).getType().dyn_cast<ShapedType>();
+  assert (resultTy && "All results must be a shaped type");
+
+  unsigned rank = resultTy.getRank();
+  llvm::errs () << "RANK: " << rank << "\n";
+
+  // Construct the indexing maps needed for linalg.generic ops.
+  SmallVector<Type> bodyArgTypes;
+
+  for (Value in : op->getOperands())
+    bodyArgTypes.emplace_back(getElementTypeOrSelf(in.getType()));
+
+  SmallVector<Type> opResultTypes;
+  SmallVector<Value> initTensors;
+
+  SmallVector<Value> dynDims;
+  dynDims.resize(results.front().getType().cast<ShapedType>().getRank());
+
+  for (auto arg : op->getOperands()) {
+    auto operandTy = arg.getType().cast<ShapedType>();
+    for (int i = 0; i < operandTy.getRank(); i++) {
+      if (operandTy.isDynamicDim(i) && !dynDims[i])
+        //dynDims[i] = rewriter.create<tensor::DimOp>(loc, arg, i);
+      {
+	 llvm::errs () << "We have dynamic dimensions!\n";
+      }
+    }
+  }
+
+  SmallVector<Value> filteredDims = condenseValues(dynDims);
+
+  for (auto result : results) {
+    auto resultTy = result.getType().template cast<ShapedType>();
+    initTensors.push_back(rewriter.create<linalg::InitTensorOp>(
+        loc, filteredDims, resultTy.getShape(), resultTy.getElementType()));
+    opResultTypes.push_back(result.getType());
+  }
+
+  auto bodyResultTypes = llvm::to_vector<4>(llvm::map_range(
+      initTensors, [](Value v) { return getElementTypeOrSelf(v); }));
+
+  SmallVector<Value, 2> operands2;
+  SmallVector<AffineMap, 3> indexingMaps;
+  indexingMaps.reserve(op->getNumOperands() + bodyResultTypes.size());
+
+  // Input indexing maps may be broadcasted.
+  for (Value operand : op->getOperands()) {
+    ShapedType type = operand.getType().cast<ShapedType>();
+
+    if (type.getShape() == resultTy.getShape()) {
+      operands2.push_back(operand);
+      indexingMaps.push_back(rewriter.getMultiDimIdentityMap(rank));
+      continue;
+    }
+
+    // this is for the result
+    SmallVector<int64_t, 5> newShape;
+    SmallVector<AffineExpr, 4> affineExprs;
+    newShape.reserve(type.getRank());
+    for (const auto &it : llvm::enumerate(type.getShape())) {
+      if (it.value() == resultTy.getDimSize(it.index())) {
+        newShape.push_back(it.value());
+        affineExprs.push_back(
+            mlir::getAffineDimExpr(it.index(), rewriter.getContext()));
+      }
+    }
+
+    assert (newShape.size () == rank && "New shape must have same rank");
+    //if (newShape.size() != rank) {
+    //  operand = rewriter.create<tosa::ReshapeOp>(
+    //      loc, RankedTensorType::get(newShape, type.getElementType()), operand,
+    //      rewriter.getI64ArrayAttr(newShape));
+    //}
+
+    operands2.push_back(operand);
+    indexingMaps.push_back(AffineMap::get(
+        /*dimCount=*/type.getRank(), /*symbolCount=*/0, affineExprs,
+        rewriter.getContext()));
+  }
+
+  indexingMaps.push_back(rewriter.getMultiDimIdentityMap(rank));
+  bool didEncounterError = false;
+  auto linalgOp = rewriter.create<linalg::GenericOp>(
+      loc, opResultTypes, operands2, initTensors, indexingMaps,
+      getNParallelLoopsAttrs(rank),
+      [&](OpBuilder &nestedBuilder, Location nestedLoc, ValueRange blockArgs) {
+        Value opResult = createLinalgBodyCalculationForElementwiseOp(
+            op, blockArgs.take_front(op->getNumOperands()),
+            bodyResultTypes, rewriter);
+        if (!opResult) {
+          didEncounterError = true;
+          return;
+        }
+        nestedBuilder.create<linalg::YieldOp>(loc, opResult);
+      });
+
+  assert (!didEncounterError && "Must not encounter errors");
+
+  rewriter.replaceOp(op, linalgOp->getResults());
+
     }
   };
 }
@@ -45,11 +181,15 @@ namespace {
 struct MyPass
     : public MyPassBase<MyPass> {
 
+  void getDependentDialects (DialectRegistry &registry) const override {
+    registry.insert <linalg::LinalgDialect> ();
+  }
+
   void runOnOperation() override {
     Operation *op = getOperation();
     MLIRContext *ctx = op->getContext();
     ConversionTarget target(*ctx);
-    target.addLegalDialect<ArithmeticDialect>();
+    target.addLegalDialect<ArithmeticDialect, LinalgDialect>();
     target.addDynamicallyLegalOp<AddIOp>([&](Operation *op) {
 		    return !any_of (op->getResultTypes (), isaTensor); });
     RewritePatternSet patterns(ctx);
