47c5a8510faf59d69d38a56038662fade5321a31
Erick Eduardo Ochoa Lopez <erick.ochoalopez@xanadu.ai>
2022-08-10 12:59:06 +0000

    Fixes comment.

--- mlir/include/mlir/Conversion/ArithmeticToLinalg/ArithmeticToLinalg.h
+++ mlir/include/mlir/Conversion/ArithmeticToLinalg/ArithmeticToLinalg.h
@@ -1,4 +1,4 @@
-//===- ArithmeticToLinalg.h - Arith to LLVM dialect conversion ----*- C++ -*-===//
+//===- ArithmeticToLinalg.h - Arith to Linalg dialect conversion *- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
